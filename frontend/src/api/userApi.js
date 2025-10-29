c8474c33a396fe50b0d22d1d090fc0e9c1088cfd
Vo Van Nam <vovannam050204@gmail.com>
2025-10-29 21:07:41 +0700

    feat: add vite api url

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = 'http://localhost:3000/user';
+const API = `${import.meta.env.VITE_API_URL}/user`;
 
 export const registerUser = async (data) => {
   const res = await axios.post(`${API}/register`, data);

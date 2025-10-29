71f9b7999793a0667430dc0167c8dbb18b000a6e
Vo Van Nam <vovannam050204@gmail.com>
2025-10-29 21:41:48 +0700

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://ia03-user-registration-v8jl.onrender.com',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
